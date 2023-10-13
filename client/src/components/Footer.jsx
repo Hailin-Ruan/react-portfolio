cedbc42e9b80748e1694e35fdcfea68d1465a83b
Hailin Ruan <hlruan829@gmail.com>
2023-10-14 02:24:06 +1100

    restart

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='icons'>
+            <a href='#'>
+                <i className='fa fa-instagram'></i>
+            </a>
+            <a href='#'>
+                <i className='fa fa-youtube-square'></i>
+            </a>
+            <a href='#'>
+                <i className='fa fa-github'></i>
+            </a>
+        </div>
+    )
+}
