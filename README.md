ea2611f0c41887418a3dc9ca0fce2a114e01ad84
Hailin Ruan <hlruan829@gmail.com>
2023-10-24 16:42:46 +1100

    inital structure

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# react-portfolio
+# my-portfolio
+
+## image
+
+## Table of Contents
+- [Description](#description)
+- [Link](#link)
+- [Built With](#built-with)
+- [Getting Started](#getting-started)
+- [Installation](#installation)
+
+## Description
+
+
+
+## Link
+
+This is the link to my portfolio: <p><a href="https://just-another-text-edit0r-bd37dce72f66.herokuapp.com/">Hailin's Portfolio</a></p>
+
+## Built With 
+
+<p><a href="https://nodejs.org/">Node.js</a></p>
+<p><a href="https://www.npmjs.com/">NPM</a></p>
+<p><a href="https://www.npmjs.com/package/express">Express.js</a></p>
+<p><a href="https://webpack.js.org/">Webpack</a></p>
+<p><a href="https://babeljs.io/">Babel</a></p>
+
+## Installation
+
+Open terminal or command prompt and run the following commands:
+* run ```npm install``` to ensure all packages are installed
+* ```npm install react-router-dom```
+* ```npm install @fortawesome/react-fontawesome @fortawesome/free-brands-svg-icons``` to install Font Awesome icon packages
+* ```npm start``` to run the app in development mode (open http://localhost:3000 to view)
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Sickie.com",
+    subtitle: "Interactive full-stack",
+    description:
+      "This app allows users to request a doctor to write a medical certificate. Doctors are also able to log in and approve the requests.",
+    image: "./project-1.gif",
+    link: "https://takesickies-40fabe44386a.herokuapp.com/",
+  },
+  {
+    title: "Timed Quiz",
+    subtitle: "HTML and CSS powered by Javascript",
+    description:
+      "This is a timed coding quiz with multiple-choice questions",
+    image: "./project-2.gif",
+    link: "https://hailin-ruan.github.io/timed-quiz/",
+  },
+  {
+    title: "Movie Buff",
+    subtitle: "Interactive front-end app",
+    description:
+      "Movie Buff is a movie search app that allows users to search for movies by title and provides detailed information such as movie posters, plot summaries, cast details, and trailers for users to watch.",
+    image: "./project-3.gif",
+    link: "https://bemonn.github.io/Movie-buff/",
+  },
+  {
+    title: "Scheduler",
+    subtitle: "HTML and CSS powered by jQuery",
+    description:
+      "This is a daily schedule application that allows users to save events for each hour of the working day (9am - 5pm)",
+    image: "./project-4.gif",
+    link: "https://hailin-ruan.github.io/Scheduler/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
